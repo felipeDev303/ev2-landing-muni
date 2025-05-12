f755d3d856524637ab6c1d866e149c69606deaf5
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-05-11 21:15:11 -0400

    feat: create main App component with navbar, hero, actions and tourism sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css"; // Tus estilos globales si los tienes
+import "./App.css"; // Estilos globales
 import MultiLevelNavbar from "./MultiLevelNavbar";
 import HeroSection from "./HeroSection";
 import HeroActions from "./HeroActions";
@@ -6,12 +6,7 @@ import HeroActions from "./HeroActions";
 import TouristCarousel from "./TouristCarousel";
 import ContactForm from "./ContactForm";
 import Footer from "./Footer";
-import { Button, Typography } from "@material-tailwind/react";
-import {
-  MagnifyingGlassIcon,
-  CalendarDaysIcon,
-  BriefcaseIcon,
-} from "@heroicons/react/24/outline"; // Importar iconos
+
 
 function App() {
 
